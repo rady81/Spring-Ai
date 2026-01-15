4886c6524c12af423078e0a696bf9122141f0803
prathyusha <prathyu86.g@gmail.com>
2026-01-15 10:08:36 -0500

    Fixed errors and added the bean conflict - config class

---
+++ 04-chat-anthropic-claude/src/main/java/com/ailabs/onechatmodule/AiClientsConfig.java
@@ -0,0 +1,32 @@
+package com.ailabs.onechatmodule;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AiClientsConfig {
+
+    @Bean("anthropicClient")
+    public ChatClient anthropicClient(@Qualifier("anthropicChatModel") ChatModel model) {
+        return ChatClient.builder(model)
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .build();
+    }
+
+    // ✅ This bean is created ONLY if OpenAI api-key is present in properties
+    @Bean("openAiClient")
+    @ConditionalOnProperty(
+            prefix = "spring.ai.openai",
+            name = "api-key"
+    )
+    public ChatClient openAiClient(@Qualifier("openAiChatModel") ChatModel model) {
+        return ChatClient.builder(model)
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .build();
+    }
+}
--- 04-chat-anthropic-claude/src/main/java/com/ailabs/onechatmodule/controller/ChatController.java
+++ 04-chat-anthropic-claude/src/main/java/com/ailabs/onechatmodule/controller/ChatController.java
@@ -3,7 +3,9 @@ package com.ailabs.onechatmodule.controller;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +19,15 @@ public class ChatController {
 
     private final ChatClient chatClient;
 
-    public ChatController(ChatClient.Builder builder) {
-        this.chatClient = builder
-                //advisor / interceptor mechanism (AOP for AI calls.)
-                .defaultAdvisors(new SimpleLoggerAdvisor())
-                .build();
+    public ChatController(@Qualifier("anthropicClient") ChatClient chatClient) {
+//        this.chatClient = builder
+//                //advisor / interceptor mechanism (AOP for AI calls.)
+//                .defaultAdvisors(new SimpleLoggerAdvisor())
+//                .build();
+        this.chatClient = chatClient;
     }
 
+    //http://localhost:8080/claude-ai/chat
     @PostMapping("/claude-ai/chat")
     Output chat(@RequestBody @Valid Input input) {
         // these sys to show as preset? true length: xx number (other than 0)
