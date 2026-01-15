c0486bc2270d2a5f1d4fa694743226a1f848de7b
prathyusha <prathyu86.g@gmail.com>
2026-01-15 13:02:18 -0500

    Prompt Template types

---
+++ 05-prompt-template-type/src/main/java/com/ailabs/prompttemplate/PromptTemplateAiApplication.java
@@ -0,0 +1,14 @@
+package com.ailabs.prompttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptTemplateAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                PromptTemplateAiApplication.class, args);
+    }
+
+}
---
+++ 05-prompt-template-type/src/main/java/com/ailabs/prompttemplate/controller/ChatController.java
@@ -0,0 +1,101 @@
+package com.ailabs.prompttemplate.controller;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import org.springframework.core.io.Resource;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+@RestController
+@RequestMapping("/")
+public class ChatController {
+
+    private final ChatClient chatClient;
+    private final Resource tweetSystemMsgResource;
+
+    public ChatController(ChatClient.Builder builder,
+                          @Value("classpath:prompts/tweet-system-message.st") Resource tweetSystemMsgResource) {
+        this.chatClient = builder
+                //advisor / interceptor mechanism (AOP for AI calls.)
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .build();
+        this.tweetSystemMsgResource = tweetSystemMsgResource;
+    }
+
+   //http://localhost:8080/claude-ai/chat
+    @PostMapping("/ollamallm-prompt-ai/chat")
+    Output chat(@RequestBody @Valid Input input) {
+        // these sys to show as preset? true length: xx number (other than 0)
+        String key = System.getenv("OPENROUTER_API_KEY");
+        System.out.println("API_KEY present? " + (key != null));
+        System.out.println("API_KEY length: " + (key == null ? 0 : key.length()));
+
+//        String response = chatClient.prompt(input.prompt()).call().content();
+//        return new Output(response);
+
+        //String systemPrompt = "You are a friendly, helpful assistant. You always respond professionally.";
+        String systemPrompt = "You are a funny, and helpful assistant. You always respond in a sarcastic manner.";
+        SystemMessage systemMessage = new SystemMessage(systemPrompt);
+        UserMessage userMessage = new UserMessage(input.prompt());
+
+        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
+        String response = chatClient.prompt(prompt).call().content();
+
+        return new Output(response);
+    }
+    public record Input(@NotBlank String prompt){};
+    public record Output(String content) {};
+
+    public record TitleSuggestionsRequest(@NotBlank String topic, @NotNull Integer count) {};
+
+    @PostMapping("/api/suggest-titles")
+    Output suggestTitles(@RequestBody @Valid TitleSuggestionsRequest request) {
+
+        String response;
+
+        PromptTemplate pt = new PromptTemplate("""
+        I would like to give a presentation about the following:
+        
+        {topic}
+        
+        Give me {count} title suggestions for this topic.
+        
+        Make sure the title is relevant to the topic and it should be a single short sentence.
+        """);
+
+        Map<String, Object> vars = Map.of("topic", request.topic(), "count", request.count());
+        Message message = pt.createMessage(vars);
+        response = chatClient.prompt().messages(message).call().content();
+
+        return new Output(response);
+    }
+
+    @PostMapping("/api/gen-tweet")
+    Output generateTweet(@RequestBody @Valid Input input) throws IOException {
+        String systemPrompt = tweetSystemMsgResource.getContentAsString(UTF_8);
+        SystemMessage systemMessage = new SystemMessage(systemPrompt);
+        UserMessage userMessage = new UserMessage(input.prompt());
+
+        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
+        String response = chatClient.prompt(prompt).call().content();
+
+        return new Output(response);
+    }
+}
